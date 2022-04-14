ceff8f7979b43ba8fc7153dedd884957449d23c8
emmacintosh <emmammrussell@gmail.com>
2022-04-13 20:22:54 -0600

    add numbers

--- variables.js
+++ variables.js
@@ -1,4 +1,11 @@
 variables assigned to strings:
-name
-favorite animal
-pizza topping
+var name = "emma"
+var favoriteAnimal = "rabbit"
+var pizzaTopping = "pepperoni"
+
+variables assigned to numbers:
+var pizzasOrdered = 2
+var age = 26
+var catsPetted = 1000
+
+variables assigned to 
